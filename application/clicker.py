d9ee00e3a64959796cd3ba8d48930bd751eae97c
Kyrylo Kusovskyi <kkirilll2006@gmail.com>
2026-05-15 14:40:44 +0300

    Cut application to more files to implement basic architecture of application

---
+++ application/clicker.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from screens.menu import *
+from screens.active_screens.user_screens import *
+from kivy.core.window import Window
+
+
+def set_window_size(x, y):
+    Window.size = (x, y)
+
+class MediumApp(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(Menu(name="menu"))
+        sm.add_widget(Game(name="game"))
+        sm.add_widget(Settings(name="settings"))
+        return sm
--- main.py
+++ main.py
@@ -1,20 +1,9 @@
-from kivy.app import App
-from screens.menu import *
-from screens.active_screens.user_screens import *
-from kivy.core.window import Window
+from application import clicker
 
-Window.size = (450, 900)
-
-
-class MediumApp(App):
-    def build(self):
-        sm = ScreenManager()
-        sm.add_widget(Menu(name="menu"))
-        sm.add_widget(Game(name="game"))
-        sm.add_widget(Settings(name="settings"))
-        return sm
-
+def main():
+    clicker.set_window_size(455, 900)
+    app = clicker.MediumApp()
+    app.run()
 
 if __name__ == '__main__':
-    app = MediumApp()
-    app.run()
+    main()
